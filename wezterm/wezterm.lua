d1d764af1ad5375a049c99c99f4db653f522b438
senntou <kane.fkjr@gmail.com>
2025-03-31 00:00:11 +0900

    updated wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,6 +1,10 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
+-- システムで使用可能なfontを確認したいとき
+-- wezterm ls-fonts --list-system
+config.font = wezterm.font("HackGen Console NF", { weight = "Regular", stretch = "Normal", style = "Normal" })
+
 config.automatically_reload_config = true
 config.font_size = 13.0
 config.window_background_opacity = 0.85
@@ -10,6 +14,16 @@ config.window_decorations = "RESIZE"
 
 
 -- WSL ONLY
--- config.default_prog = {'ubuntu.exe'}
+-- config.default_domain = 'WSL:Ubuntu-24.04'
+
+config.keys = {
+  {
+    key = 'v',
+    mods = 'CTRL',
+    action = wezterm.action {
+      PasteFrom = 'Clipboard',
+    },
+  }
+}
 
 return config
